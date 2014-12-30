3dc1e6f56eaf59f89a73f900125ec45c354dcfdc
adriansoghoian <adriansoghoian@gmail.com>
2014-12-30 16:32:59 -0500

    Multithreading working, going to run some benchmarks and see how this actually improves things.

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from scrape_repos import get_user_file_list
 from scan_text import detect_keys_in_file
 from datetime import datetime
+from threading import Thread
+from Queue import Queue
 
 
 def main(list_of_users):
+    q = Queue(maxsize=0)
+    num_threads = 0
     for user_file in list_of_users:
         results = []
         with open(user_file.split(".")[0] + "_results.csv", "wb") as r:
             with open(user_file, "rb") as f:
                 for line in f.xreadlines():
                     start = datetime.now()
-                    try:
-                        user_files = get_user_file_list(username=line.rstrip())
-                        results.append(detect_keys_in_file(user_files))
-                        print "Time it took for %s was %s" % (line, str(datetime.now() - start))
-                    except Exception as e:
-                        print e
+                    q.put(line.rstrip().split(",")[0])
+                    num_threads += 1
+                for i in range(num_threads):
+                    t = Thread(target=threadable, args=(results, q))
+                    t.setDaemon(True)
+                    print "Starting thread number: ", i
+                    t.start()
+                q.join()
             for result in results:
                 keys = result.keys()
                 for key in keys: 
@@ -23,6 +29,16 @@ def main(list_of_users):
     return results
 
 
+def threadable(results, q):
+    while True:
+        username = q.get()
+        user_files = get_user_file_list(username)
+        results.append(detect_keys_in_file(user_files))
+        q.task_done()
+
+
 if __name__ == "__main__":
-	file_paths = ["data/user_list.csv"]
-	main(file_paths)
+    start = datetime.now()
+    file_paths = ["data/user_list.csv"]
+    main(file_paths)
+    print "Time took:", str(datetime.now() - start) 
--- seed_users.py
+++ seed_users.py
@@ -4,21 +4,21 @@ from datetime import datetime
 
 
 def get_batch_users(since_index=0):
-		endpoint = "https://api.github.com/users?access_token=" + git_access_token + "&since=" + str(since_index)
-		response = urllib2.urlopen(endpoint)
-		users = json.load(response)
-		users_list = [ user['login'] for user in users ]
-		last_id = users[-1]['id']
-		return users_list, last_id
+    endpoint = "https://api.github.com/users?access_token=" + git_access_token + "&since=" + str(since_index)
+    response = urllib2.urlopen(endpoint)
+    users = json.load(response)
+    users_list = [ (user['login'], user['id']) for user in users ]
+    return users_list
 
 
 def loop_over_github(since_index=0, num_calls=4999, results_directory="data/"):
-		i = since_index
-		with open(results_directory + "user_list_" + str(since_index) + ".csv", 'wb') as f:
-				for call in range(num_calls):
-						users_list, i = get_batch_users(i)
-						for each in users_list:
-								f.write(each + "\n")
+    i = since_index
+    with open(results_directory + "user_list_" + str(since_index) + ".csv", 'wb') as f:
+        for call in range(num_calls):
+            users_list = get_batch_users(i)
+            i = users_list[-1][1]
+            for each in users_list:
+                f.write(each[0] + "," + str(each[1]) + "\n")
 
 
 if __name__ == "__main__":
